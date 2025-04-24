b0383c5361bc1c9e814f5066243c866fe0adca95
kalp234 <130490596+kalp234@users.noreply.github.com>
2025-04-24 12:10:24 +0530

    change4

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -19,7 +19,7 @@ const Banner = () => {
 
   return (
     <div className="relative w-full md:pt-[5.4rem] pt-[5rem]" name="Home">
-      <div className="relative h-[36rem] overflow-hidden rounded-sm md:h-[85vh]">
+      <div className="relative h-[26rem] overflow-hidden rounded-sm md:h-[85vh]">
       
         {currentIndex === 0 && (
           <div className="relative w-full h-full">
