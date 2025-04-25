f7b4efc2389bf6e99007bdbe354bb45de6a3d46b
kalp234 <130490596+kalp234@users.noreply.github.com>
2025-04-26 00:32:59 +0530

    change5

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -1,22 +1,24 @@
-import React from 'react';
-import './App.css';
-import 'flowbite';
-import Banner from './Components/Banner';
-import Aboutus from './Components/Aboutus';
-import Services from './Components/Services';
-import  Mutualfund  from './Components/Mutualfund';
-import LIC from './Components/LIC';
-import Footer from './Components/Footer'
+import React from "react";
+import "./App.css";
+import "flowbite";
+import Banner from "./Components/Banner";
+import Aboutus from "./Components/Aboutus";
+import Services from "./Components/Services";
+import Mutualfund from "./Components/Mutualfund";
+import LIC from "./Components/LIC";
+import Footer from "./Components/Footer";
+import ScrollToTopButton from "./Components/Scrolltotopbutton";
 
 function Homepage() {
   return (
     <div className=" App" name="Home">
-      <Banner/>
-      <Aboutus/>
-      <Services/>
-      <Mutualfund/>
-<LIC/>
+      <Banner />
+      <Aboutus />
+      <Services />
+      <Mutualfund />
+      <LIC />
       <Footer />
+      <ScrollToTopButton />
     </div>
   );
 }
