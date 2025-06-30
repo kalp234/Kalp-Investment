261820c2d3fec4101ac9410d5016937326f26d44
kalp234 <130490596+kalp234@users.noreply.github.com>
2025-06-30 22:43:28 +0530

    Stage 1 Finished

--- src/Components/SIPCalculator.jsx
+++ src/Components/SIPCalculator.jsx
@@ -81,7 +81,8 @@ const SIPCalculator = () => {
         className={`mt-12 bg-white rounded-lg shadow-xl mx-auto transition-all duration-500 flex flex-col md:flex-row p-6 gap-8 ${
           hasCalculated
             ? " max-w-[21rem] mr-[2.5rem] ml-[1.5rem] md:mr-[5rem] md:ml-[5rem] md:max-w-5xl"
-            : " max-w-[20rem] mr-[1.5rem] ml-[1.5rem] lg:mr-[27rem] lg:ml-[25rem] md:mr-[22rem] md:ml-[22rem] md:max-w-md items-center text-center"
+            : " max-w-[20rem] mx-auto md:max-w-md md:mx-[22rem] lg:mx-[25rem] items-center text-center"
+
         }`}
       >
         {/* Left: Form */}
